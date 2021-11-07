89afad5b88d72b7582541d614c1c083cc837e91f
Naoya Otsuka <otknoy@gmail.com>
2021-11-07 23:27:54 +0900

    reverse proxy sample

---
+++ go.mod
@@ -0,0 +1,3 @@
+module chproxy
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+)
+
+func main() {
+	director := func(req *http.Request) {
+		log.Printf("%#v\n", req)
+	}
+
+	modifier := func(res *http.Response) error {
+		s := "sample"
+
+		res.Body = io.NopCloser(strings.NewReader("sample"))
+		res.ContentLength = int64(len(s))
+
+		return nil
+	}
+
+	rp := &httputil.ReverseProxy{
+		Director:       director,
+		ModifyResponse: modifier,
+	}
+
+	server := http.Server{
+		Addr:    ":9000",
+		Handler: rp,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err.Error())
+	}
+}
