dfb9b58c066700f77f3bb786847f0260973a91db
Naoya Otsuka <otknoy@gmail.com>
2021-11-09 21:20:18 +0900

    add thread and res models

---
+++ model/model.go
@@ -0,0 +1,35 @@
+package model
+
+type Thread struct {
+	Title   string
+	ResList []Res
+}
+
+type Res struct {
+	Name  string
+	EMail string
+	Date  string
+	ID    string
+	Text  string
+}
+
+func (t *Thread) ToDat() (dat string) {
+	for i := 0; i < len(t.ResList); i++ {
+		row := t.ResList[i].ToDatRow()
+
+		if i == 0 {
+			dat += row + t.Title + "\n"
+			continue
+		}
+
+		dat += row + "\n"
+	}
+
+	dat += "あぼーん<>あぼーん<>あぼーん<>あぼーん<>\n"
+
+	return dat
+}
+
+func (r *Res) ToDatRow() string {
+	return r.Name + "<>" + r.EMail + "<>" + r.Date + "ID:" + r.ID + "<>" + r.Text + "<>"
+}
