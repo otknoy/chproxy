9979fb17d7d96dc3cab2229946f68ee850bfe0cc
Naoya Otsuka <otknoy@gmail.com>
2021-11-09 12:53:07 +0900

    refactor

--- proxy/server.go
+++ proxy/server.go
@@ -9,20 +9,8 @@ import (
 )
 
 func NewServer() *http.Server {
-	director := func(req *http.Request) {
-		log.Printf("%#v\n", req)
-	}
-
-	modifier := func(res *http.Response) error {
-		log.Printf("%#v\n", res)
-
-		s := "sample"
-
-		res.Body = io.NopCloser(strings.NewReader(s))
-		res.ContentLength = int64(len(s))
-
-		return nil
-	}
+	director := NewDirectory()
+	modifier := NewModifyResponse()
 
 	rp := &httputil.ReverseProxy{
 		Director:       director,
@@ -32,5 +20,24 @@ func NewServer() *http.Server {
 	return &http.Server{
 		Addr:    ":9000",
 		Handler: rp,
+	}
+}
+
+func NewDirectory() func(*http.Request) {
+	return func(req *http.Request) {
+		log.Printf("%#v\n", req)
+	}
+}
+
+func NewModifyResponse() func(*http.Response) error {
+	return func(res *http.Response) error {
+		log.Printf("%#v\n", res)
+
+		s := "sample"
+
+		res.Body = io.NopCloser(strings.NewReader(s))
+		res.ContentLength = int64(len(s))
+
+		return nil
 	}
 }
