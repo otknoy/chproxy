c5cdf6632169fe4b66fadfbffb87f4e3ce3f7ef3
Naoya Otsuka <otknoy@gmail.com>
2021-11-30 21:42:23 +0900

    fix date and id format

--- app/model/model_test.go
+++ app/model/model_test.go
@@ -14,14 +14,14 @@ func TestThread(t *testing.T) {
 			{
 				Name:  "author1",
 				EMail: "author1@example.com",
-				Date:  "",
+				Date:  "2021/11/30(火) 21:41:25",
 				ID:    "author1-id",
 				Text:  "foobar",
 			},
 			{
 				Name:  "author2",
 				EMail: "author2@example.com",
-				Date:  "",
+				Date:  "2021/11/30(火) 21:42:00",
 				ID:    "author2-id",
 				Text:  "改行は\n変換する",
 			},
@@ -30,8 +30,8 @@ func TestThread(t *testing.T) {
 
 	got := thread.ToDat()
 
-	want := `author1<>author1@example.com<>ID:author1-id<>foobar<>test-title
-author2<>author2@example.com<>ID:author2-id<>改行は <br> 変換する<>
+	want := `author1<>author1@example.com<>2021/11/30(火) 21:41:25 ID:author1-id<>foobar<>test-title
+author2<>author2@example.com<>2021/11/30(火) 21:42:00 ID:author2-id<>改行は <br> 変換する<>
 あぼーん<>あぼーん<>あぼーん<>あぼーん<>
 `
 
