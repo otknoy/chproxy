dd828aeee00d7a8878a4d0f87a5fbbb34d28253a
Naoya Otsuka <otknoy@gmail.com>
2021-12-03 21:38:58 +0900

    add BE to model

--- app/model/model_test.go
+++ app/model/model_test.go
@@ -16,6 +16,7 @@ func TestThread(t *testing.T) {
 				EMail: "author1@example.com",
 				Date:  "2021/11/30(火) 21:41:25",
 				ID:    "author1-id",
+				BE:    "author1-be",
 				Text:  "foobar",
 			},
 			{
@@ -30,7 +31,7 @@ func TestThread(t *testing.T) {
 
 	got := thread.ToDat()
 
-	want := `author1<>author1@example.com<>2021/11/30(火) 21:41:25 ID:author1-id<>foobar<>test-title
+	want := `author1<>author1@example.com<>2021/11/30(火) 21:41:25 ID:author1-id BE:author1-be<>foobar<>test-title
 author2<>author2@example.com<>2021/11/30(火) 21:42:00 ID:author2-id<>改行は <br> 変換する<>
 あぼーん<>あぼーん<>あぼーん<>あぼーん<>
 `
